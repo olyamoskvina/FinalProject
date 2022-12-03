e53acd3d6ac307005e141dc206c4aaeb9d8cc03f
Оля <moskvinaov5@gmail.com>
2022-12-04 00:49:31 +0300

    added main task

---
+++ Task/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] CreateArray()
+{
+    string[] array = new string[5];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+string[] myArray = CreateArray();
+
+string[] newArray = new string[myArray.Length];
+
+void SecondArray(string[] myArray, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+    if(myArray[i].Length <= 3)
+        {
+        newArray[count] = myArray[i];
+        count++;
+        }
+    }
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {                                                     
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+SecondArray(myArray, newArray);
+ShowArray(newArray);
